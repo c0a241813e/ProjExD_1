53de20c8f2c07402efefc1b150ba49cf3772dd1c
nasu <c0a24183e@edu.teu.ac.jp>
2025-11-18 17:57:49 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -27,17 +27,35 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200,0])
 
+
+
         key_lst = pg.key.get_pressed()
+        kx = 0
+        ky = 0
+
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            ky = +1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            ky = -1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            kx = -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            kx = +1
         else:
-            kk_rct.move_ip((-1,0))
+            kx = -1
+        
+        kk_rct.move_ip((kx,ky))
+
+        # if key_lst[pg.K_UP]:
+        #     kk_rct.move_ip((0,-1))
+        # if key_lst[pg.K_DOWN]:
+        #     kk_rct.move_ip((0,+1))
+        # if key_lst[pg.K_LEFT]:
+        #     kk_rct.move_ip((-1,0))
+        # if key_lst[pg.K_RIGHT]:
+        #     kk_rct.move_ip((+1,0))
+        # else:
+        #     kk_rct.move_ip((-1,0))
 
         screen.blit(kk_img, kk_rct)
 
