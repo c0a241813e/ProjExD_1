e6e6aaca752d7bceb554356298f32f0e8bc27a63
nasu <c0a24183e@edu.teu.ac.jp>
2025-11-18 17:24:07 +0900

    練習５背景動く

--- pygame_image.py
+++ pygame_image.py
@@ -20,12 +20,12 @@ def main():
 
         x = tmr
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200])
 
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
